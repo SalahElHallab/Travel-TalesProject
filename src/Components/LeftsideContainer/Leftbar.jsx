bb23ddb83c462524e25f960e3366e6eb65363197
SalahElHallab <slaahhallab@gmail.com>
2025-02-19 22:58:30 +0200

    update

--- src/Components/LeftsideContainer/Leftbar.jsx
+++ src/Components/LeftsideContainer/Leftbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./leftbar.css";
 
-export default function Leftbar(){
-    return(
-<div>left</div>
+export default function Leftbar() {
+    return (
+        <div className="Leftbar">
+            <div className="NotificationContainer">
+
+                <div style={{ display: "flex", justifyContent: "space-around" }}>
+                    <p>Notifications</p>
+                    <p style={{ color: "#aaa" }}>See all</p>
+                </div>
+            </div>
+
+
+        </div>
     )
 
 }
