756fccdb6b5bf30b4e8bd1a0bc20049c726907bd
SalahElHallab <slaahhallab@gmail.com>
2025-02-19 20:10:52 +0200

    setting up

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./navbar.css";
+
+export default function Navbar(){
+    return(
+        <div className="mainNavbar">
+            <div>
+                <p>Travel Tales</p>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <input type="text" name="" id="" />
+                </div>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <div>
+                <img src="" alt="" />
+                <p>Salah</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
