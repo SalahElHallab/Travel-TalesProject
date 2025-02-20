1d88231b66c1564bb62c427e4a8ed0aa509deba3
SalahElHallab <slaahhallab@gmail.com>
2025-02-20 22:00:51 +0200

    update

--- src/Components/LeftsideContainer/Leftbar.jsx
+++ src/Components/LeftsideContainer/Leftbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./leftbar.css";
+import notimg from "../../assets/Icons/ttnav.png";
 
 export default function Leftbar() {
     return (
@@ -10,7 +11,111 @@ export default function Leftbar() {
                     <p>Notifications</p>
                     <p style={{ color: "#aaa" }}>See all</p>
                 </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
+                    <img src={`${notimg}`} className="followinguserimage"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
+                    <img src={`${notimg}`} className="followinguserimage"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
+                    <img src={`${notimg}`} className="followinguserimage"></img>
+                    
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                    
+                </div>
+                
             </div>
+            <div className="NotificationContainer">
+
+                <div style={{ display: "flex", justifyContent: "space-around" }}>
+                    <p>Notifications</p>
+                    <p style={{ color: "#aaa" }}>See all</p>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
+                    <img src={`${notimg}`} className="followinguserimage"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
+                    <img src={`${notimg}`} className="followinguserimage"></img>
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
+                    <img src={`${notimg}`} className="followinguserimage"></img>
+                    
+                </div>
+                <div style={{display: "flex", alignItems:"center"}}>
+                    <img src={`${notimg}`} className="notificationimg"></img>
+                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
+                    <img src={`${notimg}`} className="likeimg"></img>
+                    
+                </div>
+                
+            </div>
+
 
 
         </div>
