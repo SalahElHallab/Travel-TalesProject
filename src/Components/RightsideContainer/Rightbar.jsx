90234f1a2fdaa05175b70c8099fd03afbad328de
SalahElHallab <slaahhallab@gmail.com>
2025-02-20 23:02:40 +0200

    update

--- src/Components/RightsideContainer/Rightbar.jsx
+++ src/Components/RightsideContainer/Rightbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./rightbar.css";
 import img from "../../assets/travelworld.jpg"
+import image1 from "../../assets/Icons/ttnav.png"
 
 export default function Rightbar() {
     return (
@@ -22,22 +23,18 @@ export default function Rightbar() {
                 </div>
 
             </div>
-            <div className="rightcontainer">
-                <div className="travelcontainer">
-                    <img src={`${img}`} className="travelimg"></img>
-                    <div>
-                        <p style={{ textAlign: "start", margin: "10px", marginTop: -50 }}>Travel Tales</p>
-                        <p style={{ textAlign: "start", margin: "10px", fontSize: "12px", marginTop: "-10px" }}>Are you ready?</p>
-                    </div>
-                </div>
-                <div className="travelcontainer">
-                    <img src={`${img}`} className="travelimg"></img>
-                    <div>
-                        <p style={{ textAlign: "start", margin: "10px", marginTop: -50 }}>Record your travels</p>
-                        <p style={{ textAlign: "start", margin: "10px", fontSize: "12px", marginTop: "-10px" }}>All about memories</p>
-                    </div>
-                </div>
+            <div className="rightcontainer2">
+                
+<p>Popular Tourist</p>
+<div>
+    <div>
 
+    </div>
+    <div>
+        <img src={`${image1}`} className="Profileimage"></img>
+        <p>Salah</p>
+    </div>
+</div>
             </div>
         </div>
     )
