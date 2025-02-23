83537204842611e55832fb3ed7ada8bf1609d5fc
SalahElHallab <slaahhallab@gmail.com>
2025-02-23 15:57:43 +0200

    update

--- src/Components/RightsideContainer/Rightbar.jsx
+++ src/Components/RightsideContainer/Rightbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./rightbar.css";
 import img from "../../assets/travelworld.jpg"
 import image1 from "../../assets/Icons/ttnav.png"
+import addfriend from "../../assets/Icons/addfriend.svg"
 
 export default function Rightbar() {
     return (
@@ -24,19 +25,22 @@ export default function Rightbar() {
 
             </div>
             <div className="rightcontainer2">
-                
-<p>Popular Tourist</p>
-<div>
-    <div>
 
-    </div>
-    <div>
-        <img src={`${image1}`} className="Profileimage"></img>
-        <p>Salah</p>
-    </div>
-</div>
+                <p>Popular Tourist</p>
+                    <div>
+                        <div style={{display:"flex", alignItems:"center", justifyContent:"space-between"}}>
+                            <div style={{display:'flex', alignItems:"center"}}>
+                                <img src={`${image1}`} className="Profileimage"></img>
+                                <p style={{marginLeft:"10px"}}>Salah</p>
+                            </div>
+                            <div style={{backgroundColor:"#aaa", padding:"10px",  marginRight:13, borderRadius:"50%"}}>
+                              <img src={`${addfriend}`}className="addfriend" alt="" />  
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        
     )
 
 }
