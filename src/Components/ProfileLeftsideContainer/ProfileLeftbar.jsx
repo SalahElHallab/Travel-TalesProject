8bdbe03e9c0e8319af8d82e9f66844c6d251a3a7
SalahElHallab <slaahhallab@gmail.com>
2025-03-05 10:16:24 +0200

    update

--- src/Components/ProfileLeftsideContainer/ProfileLeftbar.jsx
+++ src/Components/ProfileLeftsideContainer/ProfileLeftbar.jsx
@@ -7,58 +7,11 @@ export default function ProfileLeftbar() {
         <div className="ProfileLeftbar">
             <div className="NotificationContainer">
 
-                <div style={{ display: "flex", justifyContent: "space-around" }}>
-                    <p style={{marginLeft:"-20px"}}>Notifications</p>
-                    <p style={{ color: "#aaa" ,marginLeft:"40px"}}>See all</p>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
-                    <img src={`${notimg}`} className="likeimg"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
-                    <img src={`${notimg}`} className="followinguserimage"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
-                    <img src={`${notimg}`} className="likeimg"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
-                    <img src={`${notimg}`} className="likeimg"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
-                    <img src={`${notimg}`} className="likeimg"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
-                    <img src={`${notimg}`} className="likeimg"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
-                    <img src={`${notimg}`} className="followinguserimage"></img>
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Salah started to follow you</p>
-                    <img src={`${notimg}`} className="followinguserimage"></img>
-                    
-                </div>
-                <div style={{display: "flex", alignItems:"center"}}>
-                    <img src={`${notimg}`} className="notificationimg"></img>
-                    <p style={{marginLeft:"5px", color:"#aaa", fontSize:13, textAlign:"start", width:"120px"}}>Ahmed liked your post</p>
-                    <img src={`${notimg}`} className="likeimg"></img>
-                    
-                </div>
-                
+              
+        <div style={{display:"flex", alignItems:"center"}}>
+            <img src={`${notimg}`} className="Profilepageimage"/>
+            <p>Salah</p>
+        </div>
             </div>
             <div className="NotificationContainer">
 
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./profile.css"
 import Navbar from "../../Components/Navbar/Navbar";
 import ProfileLeftbar from "../../Components/ProfileLeftsideContainer/ProfileLeftbar";
+import MainPost from "../../Components/MainPostContainer/MainPost";
+import ProfileRightbar from "../../Components/ProfileRightsideContainer/ProfileRightbar";
 
 export default function Profile() {
     return(
         <div className="profile">
             <Navbar/>
-            <div>
+            <div className="subProfileContainer">
               <ProfileLeftbar/>
+              <MainPost/>
+              <ProfileRightbar/>
 
             </div>
         </div>
