d5ab3c5c9ca380270b24f31ca08a68616a53ebc3
SalahElHallab <slaahhallab@gmail.com>
2025-03-05 10:39:18 +0200

    u

--- src/Components/ProfileLeftsideContainer/ProfileLeftbar.jsx
+++ src/Components/ProfileLeftsideContainer/ProfileLeftbar.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import "./profileleftbar.css";
 import notimg from "../../assets/Icons/ttnav.png";
+import img1 from "../../assets/p2.jpg";
 
 export default function ProfileLeftbar() {
     return (
         <div className="ProfileLeftbar">
             <div className="NotificationContainer">
+                <img src={`${notimg}`} className="ProfilepageCover" />
 
-              
-        <div style={{display:"flex", alignItems:"center"}}>
-            <img src={`${notimg}`} className="Profilepageimage"/>
-            <p>Salah</p>
-        </div>
+                <div style={{ display: "flex", alignItems: "center", marginTop: -20 }}>
+                    <img src={`${img1}`} className="Profilepageimage" />
+                    <div>
+                        <p style={{ marginLeft: 6, marginTop: 20, color: "white", textAlign:"start" }}>Salah</p>
+                        <p style={{ marginLeft: 6, marginTop: 20, color: "white", textAlign:"start" }}>Lebanese</p>
+                    </div>
+
+                </div>
             </div>
             <div className="NotificationContainer">
 
                 <div style={{ display: "flex", justifyContent: "space-around" }}>
-                    <p style={{marginLeft:"-20px"}}>Explore</p>
-                    <p style={{ color: "#aaa", marginLeft:"40px"}}>See all</p>
+                    <p style={{ marginLeft: "-20px" }}>Explore</p>
+                    <p style={{ color: "#aaa", marginLeft: "40px" }}>See all</p>
                 </div>
                 <div>
-                <img src={`${notimg}`} className="exploreimage"></img>
-                <img src={`${notimg}`} className="exploreimage"></img>
-                <img src={`${notimg}`} className="exploreimage"></img>
-                <img src={`${notimg}`} className="exploreimage"></img>
-                <img src={`${notimg}`} className="exploreimage"></img>
-                <img src={`${notimg}`} className="exploreimage"></img>
+                    <img src={`${notimg}`} className="exploreimage"></img>
+                    <img src={`${notimg}`} className="exploreimage"></img>
+                    <img src={`${notimg}`} className="exploreimage"></img>
+                    <img src={`${notimg}`} className="exploreimage"></img>
+                    <img src={`${notimg}`} className="exploreimage"></img>
+                    <img src={`${notimg}`} className="exploreimage"></img>
                 </div>
-                
-                
+
+
             </div>
 
 
