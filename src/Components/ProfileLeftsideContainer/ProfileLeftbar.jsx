699fea71bdafa08a9ea4eb248a32ca32af039784
SalahElHallab <slaahhallab@gmail.com>
2025-03-05 22:58:01 +0200

    profile update

--- src/Components/ProfileLeftsideContainer/ProfileLeftbar.jsx
+++ src/Components/ProfileLeftsideContainer/ProfileLeftbar.jsx
@@ -12,10 +12,22 @@ export default function ProfileLeftbar() {
                 <div style={{ display: "flex", alignItems: "center", marginTop: -20 }}>
                     <img src={`${img1}`} className="Profilepageimage" />
                     <div>
-                        <p style={{ marginLeft: 6, marginTop: 20, color: "white", textAlign:"start" }}>Salah</p>
-                        <p style={{ marginLeft: 6, marginTop: 20, color: "white", textAlign:"start", marginTop:-15, fontSize:13 }}>Lebanese</p>
+                        <p style={{ marginLeft: 6, marginTop: 20, color: "white", textAlign:"start", color:"black" }}>Salah</p>
+                        <p style={{ marginLeft: 6, marginTop: 20, color: "white", textAlign:"start", marginTop:-15, fontSize:13, color: "#2A5D9F" }}>Lebanese</p>
                     </div>
 
+                </div>
+                <div style={{display:'flex', justifyContent:"space-between"}}>
+                    <p style={{color:"black", marginLeft:20, fontSize:'14px'}} > Places Visited </p>
+                    <p style={{color:"black", marginRight:40, fontSize:'12px', marginTop:17}} > 1 </p>
+                </div>
+                <div style={{display:'flex', justifyContent:"space-between", marginTop:-20}}>
+                    <p style={{color:"black", marginLeft:20, fontSize:'14px'}} >Travel Buddies</p>
+                    <p style={{color:"black", marginRight:40, fontSize:'12px', marginTop:17}} > 2 </p>
+                </div>
+                <div style={{ marginTop:-20}}>
+                    <h5 style={{color:"black",  fontSize:'14px',  textAlign:"center", marginTop:40, marginBottom:-5,  fontFamily: "'Lucida Handwriting', serif"}} >Traveler's Note</h5>
+                    <p style={{color:"black",  fontSize:'14px', textAlign:"center", marginLeft:10, marginRight:10}} > Capturing the world one photo at a time while chasing the best espresso.</p>
                 </div>
             </div>
             <div className="NotificationContainer">
